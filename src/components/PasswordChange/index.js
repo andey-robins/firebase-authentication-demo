420c99002c638761d0d2a03bf00bf6033ec4e1cd
Andey <andeyrobins@gmail.com>
2020-06-03 17:25:13 -0600

    Add Password Management

--- src/components/PasswordChange/index.js
+++ src/components/PasswordChange/index.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
  
-const PasswordChange = () => (
-  <div>
-    <h1>PasswordChange</h1>
-  </div>
-);
+import { withFirebase } from '../Firebase';
+
+const INITIAL_STATE = {
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+}
+
+class PasswordChangeForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { ...INITIAL_STATE };
+    }
+
+    onSubmit = event => {
+        this.props.firebase
+            .doPasswordUpdate(this.state.passwordOne)
+            .then(() => {
+                this.setState({ ...INITIAL_STATE });
+            })
+            .catch(error => {
+                this.setState({ error });
+            });
+        
+        event.preventDefault();
+    };
+
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    render() {
+        const isInvalid = this.state.passwordOne !== this.state.passwordTwo || this.state.passwordOne === '';
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input
+                    name="passwordOne"
+                    value={this.state.passwordOne}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="New Password"
+                />
+                <input
+                    name="passwordTwo"
+                    value={this.state.passwordTwo}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+                <button disabled={isInvalid} type="submit">
+                    Reset My Password
+                </button>
+
+                {this.state.error && <p>{this.state.error.message}</p>}
+            </form>
+        )
+    }
+}
  
-export default PasswordChange;
+export default withFirebase(PasswordChangeForm);
